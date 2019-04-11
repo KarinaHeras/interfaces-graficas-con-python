9ef770d043e45a62e4b86547ce6be0a7fb8a4f34
KarinaHeras <karinaheras1847@gmail.com>
2019-04-11 18:57:46 +0200

    libreríaTkinter con Windget y Label. Para mostrar texto o imagenes

---
+++ practica_label.py
@@ -0,0 +1,14 @@
+from trinker import *
+
+root=Tk()
+
+miFrame=Frame(root, width=500, height=400)
+
+miFrame.pack()
+
+#miImagen=PhotoImage(file="")
+Label(miFrame).place(x=100, y=200)
+
+
+root.minloop()
+
